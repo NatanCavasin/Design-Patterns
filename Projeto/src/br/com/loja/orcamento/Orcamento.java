3458095892723c144b1674dbdd111de50a380ea7
NatanCavasin <natancavasin@hotmail.com>
2023-04-04 09:46:11 -0300

    Implementado o Pattern State

--- Projeto/src/br/com/loja/orcamento/Orcamento.java
+++ Projeto/src/br/com/loja/orcamento/Orcamento.java
@@ -2,13 +2,18 @@ package br.com.loja.orcamento;
 
 import java.math.BigDecimal;
 
+import br.com.loja.orcamento.situacao.EmAnalise;
+import br.com.loja.orcamento.situacao.SituacaoOrcamento;
+
 public class Orcamento {
 	private BigDecimal valor;
 	private int quantidadeItens;
+	private SituacaoOrcamento situacao;
 
 	public Orcamento(BigDecimal valor, int quantidadeItens) {
 		this.valor = valor;
 		this.quantidadeItens = quantidadeItens;
+		this.situacao = new EmAnalise();
 	}
 
 	public BigDecimal getValor() {
@@ -25,6 +30,30 @@ public class Orcamento {
 
 	public void setQuantidadeItens(int quantidadeItens) {
 		this.quantidadeItens = quantidadeItens;
+	}
+
+	public SituacaoOrcamento getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(SituacaoOrcamento situacao) {
+		this.situacao = situacao;
+	}
+
+	public void aplicarDescontoExtra() {
+		this.valor = this.valor.subtract(this.situacao.calcularValorDescontoExtra(this));
+	}
+
+	public void aprovar() {
+		this.situacao.aprovar(this);
+	}
+
+	public void reprovar() {
+		this.situacao.reprovar(this);
+	}
+
+	public void finalizar() {
+		this.situacao.finalizar(this);
 	}
 
 }
