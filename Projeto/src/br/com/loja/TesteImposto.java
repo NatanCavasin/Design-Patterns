9088198ada47faa1cd3e6deade9d8dc303b2d259
Natan Cavasin <natancavasin@hotmail.com>
2023-07-19 22:53:20 -0300

    Aplicado o Pattern Decorator no calculo de imposto

--- Projeto/src/br/com/loja/TesteImposto.java
+++ Projeto/src/br/com/loja/TesteImposto.java
@@ -8,14 +8,14 @@ import br.com.loja.imposto.ICMS;
 import br.com.loja.imposto.ISS;
 import br.com.loja.orcamento.Orcamento;
 
-public class Teste {
+public class TesteImposto {
 
 	public static void main(String[] args) {
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 6);
 		Orcamento orcamento2 = new Orcamento(new BigDecimal("1000"), 1);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-		System.out.println(calculadora.calcular(orcamento, new ICMS()));
-		System.out.println(calculadora.calcular(orcamento, new ISS()));
+		System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
+		System.out.println(calculadora.calcular(orcamento, new ISS(null)));
 
 		CalculadoraDeDesconto calculadoraDesconto = new CalculadoraDeDesconto();
 		System.out.println(calculadoraDesconto.calcular(orcamento));
--- Projeto/src/br/com/loja/imposto/Imposto.java
+++ Projeto/src/br/com/loja/imposto/Imposto.java
@@ -4,6 +4,22 @@ import java.math.BigDecimal;
 
 import br.com.loja.orcamento.Orcamento;
 
-public interface Imposto {
-	BigDecimal calcular(Orcamento orcamento);
+public abstract class Imposto {
+
+	private Imposto outro; // auto relacionamento, a classe com ela mesma
+
+	public Imposto(Imposto outro) {
+		this.outro = outro;
+	}
+
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImposto = realizarCalculo(orcamento);
+		BigDecimal valorDoOutroImporto = BigDecimal.ZERO;
+		if (outro != null) {
+			valorDoOutroImporto = outro.realizarCalculo(orcamento);
+		}
+		return valorImposto.add(valorDoOutroImporto);
+	}
 }
